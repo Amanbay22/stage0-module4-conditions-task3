3d944c351da1108d7ae3143e2906d059ade7756d
ertaevamanbai40 <ertaevamanbai40@gmail.com>
2022-08-08 18:22:15 +0600

    first try

--- src/main/java/school/mjc/stage0/conditions/task3/IsEnglishSymbolDeterminer.java
+++ src/main/java/school/mjc/stage0/conditions/task3/IsEnglishSymbolDeterminer.java
@@ -1,6 +1,11 @@
 package school.mjc.stage0.conditions.task3;
 
 public class IsEnglishSymbolDeterminer {
-    public void isEnglishSymbol(char symbol) {
+    public void isEnglishSymbol(char c) {
+        if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z')) {
+            System.out.println("English");
+        } else {
+            System.out.println("Non English");
+        }
     }
 }
